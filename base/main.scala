c6c81f1ed3875467e98467a42d85e580759c3424
Wei Dai <suffixtree@gmail.com>
2014-03-04 21:12:53 +0800

    Update main.scala

--- base/main.scala
+++ base/main.scala
@@ -1,13 +1,13 @@
 import java.util
 
 object Main {
-  def main(args: Array[String]) {
-  }
-
   var st = new util.StringTokenizer("")
 
   def readToken() = {
     while (!st.hasMoreTokens) st = new java.util.StringTokenizer(readLine)
     st.nextToken
+  }
+
+  def main(args: Array[String]) {
   }
 }
